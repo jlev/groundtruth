d5b6e03a5f9f3f93676778675f0c92e518c20e82
Josh Levinger <jlev@jlev.local>
2010-03-17 00:29:22 -0400

    use better layerinfo lookup

--- info/views.py
+++ info/views.py
@@ -7,9 +7,9 @@ from django.utils import simplejson as json
 from info.models import LayerInfo,Source,Citation
 
 def info_popup_view(request,model_name):
-    l = get_object_or_404(LayerInfo,content_type__name=model_name)
+    l = get_object_or_404(LayerInfo,content_type__model=model_name)
     name=l.content_type.name
-    citations = l.citations.select_related()
+    citations = l.citations.all() #.select_related()
     return render_to_response('layer_popup.html',
         dict(layer=l,name=name,citations=citations),
         context_instance = RequestContext(request))
